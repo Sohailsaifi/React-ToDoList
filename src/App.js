1fa2b8a75da1c041ea5ed1f07eb7292f89e0fe3f
Sohail Saifi <sohailsaifi65@gmail.com>
2020-09-25 01:18:59 +0530

    Heading with CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
- 
+      <header>
+        <h1> Todo List </h1>
+      </header>
+     
+
     </div>
   );
 }
