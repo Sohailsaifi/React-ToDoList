69a29d2fdb4f22929d63811a22067de27fdbd716
Sohail Saifi <sohailsaifi65@gmail.com>
2020-09-25 01:23:35 +0530

    New component Form Added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 
+//Importing components
+import Form from './components/Form';
+
 function App() {
   return (
     <div className="App">
       <header>
         <h1> Todo List </h1>
       </header>
-     
+     <Form/>
 
     </div>
   );
