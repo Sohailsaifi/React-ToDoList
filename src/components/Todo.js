edf21779295e100c6e135e2d1167124c4970df45
Sohail Saifi <sohailsaifi65@gmail.com>
2020-10-03 22:09:34 +0530

    deletehandler added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 
-const Todo = () => {
+const Todo = ({text,todo, todos, setTodos}) => {
+
+    const deleteHandler = () => {
+        setTodos(todos.filter((el) => el.id !== todo.id));
+    };
    
     return (
         <div className="todo">
